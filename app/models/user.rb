357aa479416bbcc77b7fa44be7554cebc9cb742f
Ihor Sikora <sikoraigo@gmail.com>
2016-08-29 01:23:51 +0300

    change devise_token_auth validation to use email field as username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,34 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable
   include DeviseTokenAuth::Concerns::User
+
+
+
+  validates :email, presence: true, length: { minimum: 4, maximum: 25 }, if: Proc.new { |u| u.provider == 'email' }
+  validates_presence_of :uid, if: Proc.new { |u| u.provider != 'email' }
+
+  # only validate unique emails among email registration users
+  validate :unique_email_user, on: :create
+
+  # keep uid in sync with email
+  before_save :sync_uid
+  before_create :sync_uid
+
+
+  protected
+
+  # only validate unique email among users that registered by email
+  def unique_email_user
+    if provider == 'email' and self.class.where(provider: 'email', email: email).count > 0
+      errors.add(:email, I18n.t("errors.messages.already_in_use"))
+    end
+  end
+
+  def sync_uid
+    self.uid = email if provider == 'email'
+  end
 end
+
