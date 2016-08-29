6ebaedb4af935c0554410f9f56991cc5df941eac
Ihor Sikora <sikoraigo@gmail.com>
2016-08-29 03:38:54 +0300

    add Pearson correlation coefficient check

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  post '/datasets', to: 'datasets#analisis'
+  post '/analisis',    to: 'datasets#analisis'
+  post '/correlation', to: 'datasets#correlation'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
