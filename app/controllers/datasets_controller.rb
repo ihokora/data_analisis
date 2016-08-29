de99d16a41e9f32bdc18220e8621bdf6adf14d4a
Ihor Sikora <sikoraigo@gmail.com>
2016-08-29 10:46:13 +0300

    add Tukey outliers filter to analisis

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -5,6 +5,10 @@ class DatasetsController < ApplicationController
     data_x = dataset.data_x.split(",").map(&:to_i)
     stats = DescriptiveStatistics::Stats.new(data_x)
     stats = stats.descriptive_statistics
+    q1 = stats[:q1]
+    q3 = stats[:q3]
+    outliers = outliers_check(data_x, q1, q3 )
+    stats[:outliers] = outliers
     render json: stats
   end
 
@@ -26,25 +30,36 @@ class DatasetsController < ApplicationController
       params.permit(:data_x, :data_y)
     end
 
+    # Findind Outliers in Data with the Tukey Method
+    def outliers_check(data, q1, q3)
+      irq         = q3 - q1
+      lower_fence = q1 - (irq * 1.5)
+      upper_fence = q3 + (irq * 1.5)
+      lower_outliers = data.select { |num| num < lower_fence}
+      upper_outliers = data.select { |num| num > upper_fence }
+      outliers = lower_outliers + upper_outliers
+    end
+
+
     def ruby_pearson(x,y)
-      n=x.length
+      n = x.length
 
-      sumx=x.inject(0) {|r,i| r + i}
-      sumy=y.inject(0) {|r,i| r + i}
+      sumx = x.inject(0) {|r,i| r + i}
+      sumy = y.inject(0) {|r,i| r + i}
 
-      sumxSq=x.inject(0) {|r,i| r + i**2}
-      sumySq=y.inject(0) {|r,i| r + i**2}
+      sumxSq = x.inject(0) {|r,i| r + i**2}
+      sumySq = y.inject(0) {|r,i| r + i**2}
 
-      prods=[]; x.each_with_index{|this_x,i| prods << this_x*y[i]}
-      pSum=prods.inject(0){|r,i| r + i}
+      prods = []; x.each_with_index{|this_x,i| prods << this_x*y[i]}
+      pSum = prods.inject(0){|r,i| r + i}
 
       # Calculate Pearson score
-      num=pSum-(sumx*sumy/n)
-      den=((sumxSq-(sumx**2)/n)*(sumySq-(sumy**2)/n))**0.5
-      if den==0
+      num = pSum -(sumx*sumy/n)
+      den = ((sumxSq -(sumx**2)/n)*(sumySq-(sumy**2)/n))**0.5
+      if den == 0
         return 0
       end
-      r=num/den
+      r = num/den
       return r
     end
 end
